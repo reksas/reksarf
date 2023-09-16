b4f834d8874bff52cd414ecb0317a70e6a337278
reksas <145149970+reksas@users.noreply.github.com>
2023-09-16 13:59:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,392 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>РЕМОНТ ПЛАСТИКОВЫХ ОКОН И ДВЕРЕЙ</title>
+<meta charset="utf-8">
+<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD. The purpose of this project is to accurately emulate the PlayStation 3 in its entirety with the power of reverse engineering and community collaboration.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, home">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - The PlayStation 3 Emulator" />
+<meta property="og:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering." />
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="RPCS3" />
+<meta name="twitter:title" content="RPCS3 - The PlayStation 3 Emulator">
+<meta name="twitter:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6161192934701994" crossorigin="anonymous"></script>
+</head>
+<body>
+<img src="/img/graphics/landing/hero-main.png" width="1" height="1" alt="preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/home/inc-home-player.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'xUpw9bHmoac',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'gIcsUr7W00k',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'60lq9pXzDic',
+	'3'
+	);
+?>
+
+<div class="page-con-content landing-con-override">
+
+	<div class="wavebar-con-container-master">
+	
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		
+		
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+		
+			<div class="content-con-inside">
+			
+				<div class='landing-con-left content-expand '>
+				
+				
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class=''>
+						
+						
+						</div>
+						
+						<div class='landing-tx1-heading pulsate' >
+							<h1>Ремонт<br>
+							 пластиковых окон и дверей</h1>
+						</div>
+						<div class='landing-tx2-heading' >
+							<p>
+								 Профессиональный оконный сервис, выполняем все виды работ по ремонту окон и дверей в Красноярске.«Окно Сервис» - это команда профессионалов, знающая об окнах всё.
+Наши специалисты выполнят ремонт окон и дверей любой сложности в кратчайшие сроки по доступным ценам. У нас качественно, дешево и быстро!
+							</p>
+							<br />
+							<br />
+							<br />
+					
+						
+								
+							<font size="22" color="yellow">Контакты: <br /> </font>
+					
+							
+							<font size="22" color="yellow">+7 (983) 140-17-78</font>
+							
+							
+						
+						
+						
+						
+						
+						
+						
+							
+
+							<div class='landing-btn-container'>
+								<div class="">
+									<div class="build-ico-button">
+										<div class="build-ico-os">
+										
+										</div>
+									</div>
+									<a href="">
+									<div class="">
+										
+									</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+				<?php
+
+?>
+					<div class='landing-img-hero' style="background: url(https://oknoservice24.ru/img/s1_p1.png) no-repeat center ; background-size  : contain ;">
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="">
+		<div class="">
+		</div>
+		<div class=''>
+			<div class="">
+				<div class="">
+					<div class="">
+						<div class="">
+						</div>
+						<div class="">
+							<a href="" target="">
+							<span class=""> <span class=""></span>
+							<span class=""></span>
+							</span>
+							</a>
+						</div>
+						<form action='/compatibility' method='get'>
+							
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-video darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-btn-left darkmode-arrow-left left" href="#myCarousel" data-slide="prev">
+			</div>
+			<div class="content-btn-right darkmode-arrow-right right" href="#myCarousel" data-slide="next">
+			</div>
+			<div class="content-con-inside">
+				<div class="video-tx1-heading darkmode-txt">
+					<h2>Наши услуги</h2>
+					 <!DOCTYPE html>
+<html>
+   <title>
+    	Наука.ру - главная
+    </title>
+    <body>
+        <style>
+    	    h1{text-align:center;color:white;}
+            h3{text-align:center;color:white;}
+            body{background-image:url("https://d626yq9e83zk1.cloudfront.net/files/2017/12/26-770x425.jpg");}
+        </style>
+        
+    	<h1>Космоинфо</h1>
+        <h3>Погрузись в космос вместе с нами!</h3>
+        <h2><pre><a href = "http://www.astronews.ru">Новости</a>    <a href = "https://www.gismeteo.ru">Вопросы</a>   <a href = "file:///home/danila/program.html">Основная</a></pre></h2>
+    </body>
+</html>
+				</div>
+				<div class="">
+				</div>
+				<?php include 'lib/module/home/inc-home-carousel.php';?>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-discord darkmode-discord">
+		<div class="">
+			
+		</div>
+		<div class="discord-center-logo"src="https://cache3.youla.io/files/images/720_720_out/5d/14/5d147719074b3e68d1355ae3.jpg" style="width: 100%; height: 100%;">
+			<img alt="Discord Logo" src="https://cache3.youla.io/files/images/720_720_out/5d/14/5d147719074b3e68d1355ae3.jpg" style="width: 100%; height: 150%;">
+		</div>
+		<div class="">
+			
+		</div>
+		<div class="">
+			<div class="">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-image-logo"style="background: url(https://oknoservice24.ru/img/grafik1.jpg) no-repeat center; background-size: contain;">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>МОСКИТНЫЕ СЕТКИ ОТ ПРОИЗВОДИТЕЛЯ</h2>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									Москитные сетки по вашим размерами до 34% дешевле готовых!Размеры:30х30см 40х40см 70х70см 50х140см 50х150см 50х170см 70х140см 70х150см 70х170см 80х140см 80х150см 80х170см
+								</p>
+							</div>
+							<a href="" target="">
+							<div class="">
+								<div class="">
+								</div>
+								<div class="">
+									<span></span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.1" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<div class="">
+		<div class=''>
+			<span></span>
+		</div>
+		<div class="">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-github">
+		<div class="github-img-backdrop"style="background: url(http://cryptoconsulting.ru/wp-content/uploads/2020/09/1837.jpg) no-repeat center; background-size: contain;">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class=""style="background: url(https://e7.pngegg.com/pngimages/447/944/png-clipart-computer-icons-others-miscellaneous-image-file-formats.png) no-repeat center; background-size: contain;">
+						<div class="">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Работаем с юридическими лицами
+и организациями</h2>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									 Наличный и безналичный расчет
+Индивидуальный подход
+Офисные здания
+Государственные учреждения
+Детские сады
+Школы
+Организации любой формы собственности
+								</p>
+							</div>
+							<a href="" target="">
+							<div class="">
+								<div class="">
+								</div>
+								<div class="">
+									<span></span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+			<span></span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="https://oknoservice24.ru/img/grafik1.jpg">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo"style="background: url(https://oknoservice24.ru/img/grafik1.jpg) no-repeat center; background-size: contain;">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra">
+								<h2>СТОИМОСТЬ УСЛУГ В СРЕДНЕМ
+НА 30% НИЖЕ, ЧЕМ У КОНКУРЕНТОВ!</h2>
+								<div class="landing-con-divider" style="background: #ff426d">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									Наша компания занимается обслуживанием и ремонтом окон и дверей ПВХ уже более 15 лет. За это время мы вернули к жизни более 5 500 окон и дверей. Наши цены и качество выполняемых работ являются одними из лучших на Красноярском рынке ремонта и монтажа оконных конструкций.
+								</p>
+							</div>
+							<a href="" target="">
+							<div class="">
+								<div class="">
+								</div>
+								<div class="">
+									<span></span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature"style="background: url(https://sun9-24.userapi.com/impg/jbGvWW3M412-XkkFUm2unU1CKkADwwZxjP-gOw/L0Rz0JBuUDQ.jpg?size=2542x1152&quality=95&sign=62d6f83634ad668879391ffd92762b5a&type=album) no-repeat center  ; background-size 233% : contain  233%;">
+	
+	<div class="">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo"style="background: url() no-repeat center 100%; background-size 100%: contain 100%;">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1" >
+							<div class="patreon-tx1-mantra">
+								<h2>
+
+</h2>
+								<div class="landing-con-divider" style="background: #ff424d">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt"style="background: #F5F5DC">
+								<p>
+									
+
+
+								</p>
+</div>
+
+</body>
+</html>
+
